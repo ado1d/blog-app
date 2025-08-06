229d11ea08b842ac1c3bff80c01d1914e3b66731
ado1d <2021831024@student.sust.edu>
2025-08-07 02:55:01 +0600

    Update

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ const port = process.env.PORT || 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views')); 
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
